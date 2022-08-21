0f18659a143d72cf3c45f0e62968b46f7ec41092
Dayne Lalmond <dlalmondo@gmail.com>
2022-08-21 11:49:39 -0400

    additional styling and commenting on js

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
-// var badRequestUrl = "http://api.openweathermap.org/data";
-
+// my key is required to use the API 
 const key = "d83d9f6051a8ec040d1c4db0b4a4f22d";
 
-var now = moment();
-
+var weatherCard = document.getElementById("weather-card");
 
 function getApi(lat, lon, cityName) {
+  // variable for requesting url with inserted variables when called by the fetch
   var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly&appid=" + key;
   fetch(requestUrl)
-// two .then functions to recieve and convert, and use the data
+// two .then functions to recieve and convert, and next to use the converted information as 'data'
     .then(function(response) {
       return response.json()
     })
@@ -16,64 +15,63 @@ function getApi(lat, lon, cityName) {
 
     .then(function (data) {
       showWeatherData(data, cityName)
+      // calling showWeatherData function with 'data' and 'cityname'
       // passing cityname from geoRequest to getapi to showweatherdata
     })
 }
 
 
-// showweatherdata is part of a function chaining where i add a function at the end of another function
+// function is called in the order of variable 'submit' being clicked -> getCoordinates() -> getApi() = showWeatherData()
 function showWeatherData(data, cityName) {
 
-  // var city = data.
   var temp = data.current.temp;
   var humidity = data.current.humidity;
   var wind = data.current.wind_speed;
-document.querySelector(".called-weather").style.display = 'block';
+  // variables are used to manipulate the text of the below elements to the specified data from the API
 document.querySelector("#city").innerText = cityName;
 document.querySelector("#temp").innerText = temp + "°F"
 document.querySelector("#wind").innerText = "Wind " + wind + " MPH"
 document.querySelector("#humidity").innerText = humidity + "%  Humidity";
-
+addClass()
+// calling addClass function
 }
 
+// addclass changes the display and adds animation
+function addClass() { 
+  weatherCard.classList.add("toggler");
+  // adds a class to the 'weatherCard'
+  document.querySelector(".called-weather").style.display = 'block';
+  // changes the CSS style of the class '.called-weather'
+}
 
-
-
+// getcoordinates will find the information about the searched city
 function getCoordinates(city) {
   var geoRequestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + key
   fetch(geoRequestUrl)
       .then(function(response) {
         return response.json()
+          // fetching the url, then converting 'response' to json format
       })
       .then(function (data) {
         var lat = data[0].lat
         var lon = data[0].lon
         var cityName = data[0].name
         getApi(lat, lon, cityName)
+        // 'response' is converted to 'data' where additional parameters of the API are included per variable
+        // getApi function is called with the previously created variables
       })
 }
-// getcoordinates uses the actual cityname
 
 
+// the submit button is the variable to initialize the function chaining above
 var submit = document.getElementById('submit');
 
 submit.addEventListener("click", function(event) {
   event.preventDefault()
+  // prevent default is required for the submit button to continue without refreshing the page
   var input = document.getElementById('userInput').value;
   getCoordinates(input)
+  //getCoordinates is called by clicking 'submit' with the 'input' variable to be searched
 })
 
 
-
-
-
-
-
-
-
-// $("#weather-card" ).animate({
-//   width: [ "toggle", "swing" ],
-//   opacity: "toggle"
-//  }, 1000, "linear", function() {
-//   $( this ).after();
-//  });
